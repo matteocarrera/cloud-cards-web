31bffce11491a5636a644dd13768bad3db24737a
matteocarrera <matteocarrera@mail.ru>
2021-05-24 17:24:23 +0500

    Отображение компаний и небольшой редизайн

--- main.js
+++ main.js
@@ -8,13 +8,29 @@ var firestore = firebase.firestore();
     ID визитки
 */
 var link = window.location.hash.substring(1);
+
+if (link == "") {
+	window.location.href = window.location.href + "start.html"
+}
+
 var parentId = link.split('&')[0]
 var uuid = link.split('&')[1]
+var type = link.split('&')[2]
+if (type == undefined) {
+	type = 'personal'
+}
 
 /*
     Получение родительского пользователя
 */
-firestore.collection("users").doc(parentId).collection("data").doc(parentId).get().then(function(doc) {
+firestore
+	.collection("users")
+	.doc(parentId)
+	.collection("data")
+	.doc(parentId)
+	.get()
+	.then(function(doc) {
+
     if (doc.exists) {
         var ownerUserJson = JSON.stringify(doc.data()); 
         var ownerUser = new User(JSON.parse(ownerUserJson));
@@ -22,32 +38,52 @@ firestore.collection("users").doc(parentId).collection("data").doc(parentId).get
         /*
             Получение пользователя с конкретной визитки
         */
-        firestore.collection("users").doc(parentId).collection("cards").doc(uuid).get().then(function(doc) {
+        firestore
+        	.collection("users")
+        	.doc(parentId)
+        	.collection("cards")
+        	.doc(uuid)
+        	.get()
+        	.then(function(doc) {
+            
             if (doc.exists) {
             	var type = doc.data()["type"];
                 var userBooleanJson = null;
                 var userBoolean = null;
+                var currentUser = null;
                 switch (type) {
                     case 'personal':
                         userBooleanJson = JSON.stringify(doc.data()["data"]); 
                         userBoolean = new UserBoolean(JSON.parse(userBooleanJson));
+                        currentUser = getUserFromTemplate(ownerUser, userBoolean);
+
+		                generateVCard(currentUser);
+		                fillTableWithPersonalData(currentUser);
                         break;
+                    case 'company':
+                    	var companyJson = JSON.stringify(doc.data()["data"]); 
+                    	var company = new Company(JSON.parse(companyJson));
+                    	fillTableWithCompanyData(company);
+                    	break;
                     case undefined:
                         userBooleanJson = JSON.stringify(doc.data()); 
                         userBoolean = new UserBoolean(JSON.parse(userBooleanJson));
+                        currentUser = getUserFromTemplate(ownerUser, userBoolean);
+
+		                generateVCard(currentUser);
+		                fillTableWithPersonalData(currentUser);
                         break;
                 }
-
-                var currentUser = getUserFromTemplate(ownerUser, userBoolean);
-                generateVCard(currentUser);
-                fillTable(currentUser);
+                
 
                 if(/iPhone/i.test(navigator.userAgent)) {
                     document.getElementById('import-contact-row').classList.remove("d-none");
                 }
+
             } else {
                 console.log("Визитки по данному адресу не существует!");
             }
+
         }).catch(function(error) {
             console.log("Ошибка при получении документа: ", error);
         });
@@ -55,15 +91,20 @@ firestore.collection("users").doc(parentId).collection("data").doc(parentId).get
         /*
             Получение фотографии пользователя
         */
-        storage.ref(doc.data().photo).getDownloadURL().then(function(url) {
-            var img = document.getElementsByClassName('avatar')[0];
-            img.src = url;
-        }).catch(function(error) {
-            console.log("Ошибка при загрузке изображения: ", error);
-        });
+
+		if (type == 'personal') {
+			storage.ref(doc.data().photo).getDownloadURL().then(function(url) {
+				var img = document.getElementsByClassName('avatar')[0];
+	            img.src = url;
+	        }).catch(function(error) {
+	            console.log("Ошибка при загрузке изображения: ", error);
+	        });
+		}
+
     } else {
         console.log("Пользователя по данному адресу не существует!");
     }
+
 }).catch(function(error) {
     console.log("Ошибка при получении документа: ", error);
 });
@@ -75,6 +116,7 @@ function importContact() {
 /*
     Генерация VCard пользователя
 */
+
 function generateVCard(currentUser) {
     // TODO("Добавить фото контакта")
     var user = vCard.create(vCard.Version.THREE);
@@ -103,9 +145,9 @@ function generateVCard(currentUser) {
 /*
     Заполнение таблицы на сайте данными контакта
 */
-function fillTable(currentUser) {
-    document.getElementById('name').innerHTML = currentUser.surname + ' ' + currentUser.name + ' ' + currentUser.patronymic;
-                
+
+function fillTableWithPersonalData(currentUser) {
+    setDataToField('name-row', 'name', currentUser.surname + ' ' + currentUser.name + ' ' + currentUser.patronymic, null);
     setDataToField('mobile-row', 'mobile', currentUser.mobile, 'tel:');
     setDataToField('mobileSecond-row', 'mobileSecond', currentUser.mobileSecond, 'tel:');
     setDataToField('email-row', 'email', currentUser.email, 'mailto:');
@@ -123,10 +165,28 @@ function fillTable(currentUser) {
     setLinkToSocial('twitter', 'https://twitter.com/', currentUser.twitter);
 }
 
+/*
+    Заполнение таблицы на сайте данными компании
+*/
+
+function fillTableWithCompanyData(currentCompany) {
+
+	setDataToField('company-name-row', 'company-name', currentCompany.name, null);
+	setDataToField('company-responsible-row', 'company-responsible', currentCompany.responsibleFullName, null);
+	setDataToField('company-responsible-job-title-row', 'company-responsible-job-title', currentCompany.responsibleJobTitle, null);
+    setDataToField('mobile-row', 'mobile', currentCompany.phone, 'tel:');
+    setDataToField('email-row', 'email', currentCompany.email, 'mailto:');
+    setDataToField('address-row', 'address', currentCompany.address, 'http://maps.google.com/?q=');
+    setDataToField('site-row', 'site', currentCompany.website, '');
+}
+
+/*
+	Заполнение данных в ячейку таблицы
+*/
+
 function setDataToField(fieldRow, field, data, additionalText) {
-	if (data == '') {
-		document.getElementById(fieldRow).classList.add('d-none');
-	} else {
+	if (data != '') {
+		document.getElementById(fieldRow).classList.remove('d-none');
 		if (additionalText != null) {
 			document.getElementById(field).href = additionalText + data
 		}
@@ -134,13 +194,20 @@ function setDataToField(fieldRow, field, data, additionalText) {
 	}
 }
 
+/*
+	Заполнения ссылки на социальные сети
+*/
+
 function setLinkToSocial(socialId, socialAddress, userData) {
 	if (userData == '') {
-		document.getElementById(socialId).classList.add('d-none');
-	} else {
+		document.getElementById(socialId).classList.remove('d-none');
 		document.getElementById(socialId).href = socialAddress + userData;
 	}
 }
+
+/*
+	Заполнение социальных сетей для VCard
+*/
 
 function setSocialToVCard(user, social, key, userData) {
     if (userData != "") {
@@ -148,4 +215,9 @@ function setSocialToVCard(user, social, key, userData) {
     }
 }
 
-//Тестовая ссылка : #07D64EDB-2763-4ADD-8D98-0715F86D4D7F&AEDEAF5E-CED5-4D2D-B1DB-9DE6B41DD41E
+/*
+	Тестовые ссылки:
+		- Без типа: #07D64EDB-2763-4ADD-8D98-0715F86D4D7F&AEDEAF5E-CED5-4D2D-B1DB-9DE6B41DD41E
+		- Персональная: #07D64EDB-2763-4ADD-8D98-0715F86D4D7F&AEDEAF5E-CED5-4D2D-B1DB-9DE6B41DD41E&personal
+		- Компания: #07D64EDB-2763-4ADD-8D98-0715F86D4D7F&EE424775-E882-4D0C-BE92-DE21471F7AE7&company
+*/
